0f1f2bf8ff8326d8787ccf4be81ed72f81e29d61
Eric <makewaves360@gmail.com>
2021-01-16 10:21:22 +0530

    ok

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-
-// use \vendor\Spipu\Html2Pdf\Html2Pdf;
 require_once('vendor/autoload.php');
-
 use Spipu\Html2Pdf\Html2Pdf;
 use Spipu\Html2Pdf\Exception\Html2PdfException;
 use Spipu\Html2Pdf\Exception\ExceptionFormatter;
@@ -11,7 +8,8 @@ ob_start();
 include 'html.php';
 $content = ob_get_clean();
 
-$html2pdf = new Html2Pdf();
+$html2pdf = new Html2Pdf('P', 'A3', 'fr', true);
+$html2pdf->pdf->SetDisplayMode('fullpage');
 $html2pdf->writeHTML($content);
 $html2pdf->output('myPdf.pdf'); // Generate and load the PDF in the browser.
 
